bfd0dfa18e964e38f1b2d9826cdbb5b20c0044aa
Jonathan Walters <jonfwalters@gmail.com>
2018-05-16 22:07:05 +0100

    about page and javascript added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
+    
+   $(".btn").hover(function(){
+        $(this).css("background-color", "#e0b8d8");
+        }, function(){
+        $(this).css("background-color", "#C01E6A");
     });
-});
 
+	function changeBackground(x) {
+    	if (x.matches){
+        document.body.style.backgroundColor = "white";
+    	} else {
+        document.body.style.backgroundColor = "#F6F6F6";
+    	}
+	}
+
+	var x = window.matchMedia("(max-width: 768px)")
+	changeBackground(x) 
+	x.addListener(changeBackground)
+
+});
